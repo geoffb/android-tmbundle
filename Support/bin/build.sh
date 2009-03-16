b2bdeef6e7fdb7618c4975982e8192b07eb03826
geoffb <gmblair@gmail.com>
2009-03-15 20:58:49 -0700

    Changed Hotkey to Alt-Command-A Added Build (sans Install) command Added AppleScript which focuses the running emulator after installing apk Improved UI

--- Support/bin/build.sh
+++ Support/bin/build.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
-echo "<style type=\"text/css\">body{margin:0;} #body{padding:10px;} h1{background:#AACA46;color:#fff;padding:10px} .error{color:#f00;font-weight:bold;} pre{background:#ddd;height:150px;border:solid 1px #000;padding:10px;overflow:auto;}</style>"
-echo "<h1>Android: Building with Ant</h1>"
+
+# Android.tmbundle build script
+# Finds Ant build.xml files and builds the project
+# (re)Installs *-debug.apk files to a running Android emulator using adb
+
+printError() {
+	echo "<span class=\"error\">Error: $1</span>"
+}
+
+runCommandAndPrintOutput() {
+	echo "fpp"
+}
+
+if [ "$1" = "install" ]; then
+	TITLE="Build & Install"
+else
+	TITLE="Build"
+fi
+
+echo "<style type=\"text/css\">html,body{margin:0;background:#D4E9A9;} #body{padding:10px;} h1{background:#AACA46;color:#fff;padding:10px;border-bottom:solid 3px #aaa;} .error{color:#f00;font-weight:bold;display:block;} pre{background:#ddd;border:solid 1px #000;padding:10px;overflow:auto;}</style>"
+echo "<h1>Android: $TITLE</h1>"
 echo "<div id=\"body\">"
+
+#######################################
+## Build with Ant #####################
+#######################################
 
 ANT=`which ant`
 if [ "$ANT" = "" ]; then
-	echo "<span class=\"error\">Couldn't find Ant!</span><br>"
+	printError "Couldn't find Ant!"
 	exit
 fi
 
-cd $TM_DIRECTORY
+cd "$TM_DIRECTORY"
 
-while [ $PWD != "/" ]; do
-	if test -e $PWD/build.xml; then 
+while [ "$PWD" != "/" ]; do
+	if test -e "$PWD/build.xml"; then
 		BUILD_DIR="$PWD"
 		break
 	fi
@@ -20,32 +43,46 @@ while [ $PWD != "/" ]; do
 done
 
 if [ "$BUILD_DIR" = "" ]; then
-	echo "<span class=\"error\">Couldn't find build.xml!</span><br>"
+	printError "Couldn't find build.xml!"
 	exit
 fi
 
 cd $BUILD_DIR
 
-echo "<h2>Ant output:</h2>"
+echo "Building from $BUILD_DIR<br>"
+echo "Building with Ant...<br>"
 echo "<pre>"
-$ANT $BUILD_XML
+$ANT
 echo "</pre>"
 
-ADB=`which adb`
-if [ "$ADB" = "" ]; then
-	echo "<span class=\"error\">Couldn't find adb!</span><br>"
-	exit
+#######################################
+## Install with adb ###################
+#######################################
+
+if [ "$1" = "install" ]; then
+
+	ADB=`which adb`
+	if [ "$ADB" = "" ]; then
+		printError "Couldn't find adb!"
+		exit
+	fi
+
+	cd bin
+
+	echo "Installing with adb<br>"
+	echo "Looking for *-debug.apk files in $PWD<br>"
+	for FILE in $PWD/*-debug.apk; do
+		echo "Found debug apk: $FILE<br>"
+		CMD="$ADB install -r $FILE"
+		echo "Installing...<br>"
+		echo "<pre>"
+		$CMD
+		echo "</pre>"
+	done
+
+	osascript -e "tell application \"emulator\" to activate"
+
 fi
-
-cd bin
-
-echo "<h2>adb ouput:</h2>"
-echo "<pre>"
-for FILE in $PWD/*.apk; do
-	CMD="$ADB install -r $FILE"
-	$CMD
-done
-echo "</pre>"
 
 echo "</div><!-- #body -->"
 
